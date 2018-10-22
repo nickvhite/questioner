62bdc0d2183f35e2a6a223ba72c2318936d96c71
nickvhite <nickvhite@gmail.com>
2018-10-22 16:07:00 +0300

    Update Greeting.js
    remove class for faster work

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-class Greeting extends Component {
-    render() {
-        let greeting = this.props.eventList.greeting;
-		return (
-            <div className="greeting">
-                <p className="small_text">{greeting.text_1}</p>
-                <p className="small_text">{greeting.text_2}</p>
-                <p className="big_text">{greeting.text_3}</p>
-            </div>
-        )
-    }
-}
+const Greeting =({ eventList }) => (
+    <div className="greeting">
+        <p className="small_text">{eventList.greeting.text_1}</p>
+        <p className="small_text">{eventList.greeting.text_2}</p>
+        <p className="big_text">{eventList.greeting.text_3}</p>
+    </div>
+)
 
 export default connect(
     state => ({
